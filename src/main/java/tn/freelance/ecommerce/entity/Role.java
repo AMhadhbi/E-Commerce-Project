c0ff7c0b431395ec8b09b411f3c25142bb3f9f5f
AkremGA <akrem.gaiida@gmail.com>
2015-03-15 23:24:26 +0100

    add annotation JPA to entity user and role

--- src/main/java/tn/freelance/ecommerce/entity/Role.java
+++ src/main/java/tn/freelance/ecommerce/entity/Role.java
@@ -2,9 +2,19 @@ package tn.freelance.ecommerce.entity;
 
 import java.io.Serializable;
 
-public class Role implements Serializable {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Role implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
 	private Long idRole;
+	@Column(name = "role_name")
 	private String roleName;
 
 	public Role() {
--- src/main/java/tn/freelance/ecommerce/entity/User.java
+++ src/main/java/tn/freelance/ecommerce/entity/User.java
@@ -3,49 +3,78 @@ package tn.freelance.ecommerce.entity;
 import java.io.Serializable;
 import java.util.Collection;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
 	private Long idUser;
+	@Column(name = "user_name")
 	private String userName;
 	private String password;
 	private boolean activated;
+	@OneToMany
+	@JoinColumn(name = "user_id")
 	private Collection<Role> roles;
-	
+
 	public User() {
 		super();
 	}
+
 	public User(String userName, String password, boolean activated) {
 		super();
 		this.userName = userName;
 		this.password = password;
 		this.activated = activated;
 	}
+
 	public Long getIdUser() {
 		return idUser;
 	}
+
 	public void setIdUser(Long idUser) {
 		this.idUser = idUser;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public boolean isActivated() {
 		return activated;
 	}
+
 	public void setActivated(boolean activated) {
 		this.activated = activated;
 	}
+
 	public Collection<Role> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(Collection<Role> roles) {
 		this.roles = roles;
 	}
