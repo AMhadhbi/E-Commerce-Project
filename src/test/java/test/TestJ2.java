dfa9f99f4d57009276bf0c005cca27d3abbf91ab
AMhadhbi <mhadhbi.achref@gmail.com>
2015-03-17 20:37:24 +0100

    Test success :" add product and show listProduct"

--- src/test/java/test/TestJ2.java
+++ src/test/java/test/TestJ2.java
@@ -8,13 +8,14 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import tn.freelance.ecommerce.entity.Category;
+import tn.freelance.ecommerce.entity.Product;
 import tn.freelance.ecommerce.service.EcommerceServiceImpl;
 import tn.freelance.ecommerce.service.IAdminCategoryService;
 
 public class TestJ2 {
 
 	@Test
-	public void test() {
+	public void testCategroy() {
 		try {
 			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 					new String[] { "applicationContext.xml" });
@@ -24,10 +25,27 @@ public class TestJ2 {
 			List<Category> cats1 = metier.listCategory();
 			metier.addCategory(new Category("CAT1"));
 			metier.addCategory(new Category("CAT2"));
-			metier.addCategory(new Category("CAT2"));
 			List<Category> cats2 = metier.listCategory();
-
 			assertTrue(cats2.size() == cats1.size() + 2);
+		} catch (Exception e) {
+			assertTrue(e.getMessage(), false);
+		}
+
+	}
+
+	@Test
+	public void testProduct() {
+		try {
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+					new String[] { "applicationContext.xml" });
+
+			IAdminCategoryService metier = (IAdminCategoryService) context
+					.getBean("metier");
+			List<Product> prod1 = metier.listproduct();
+			metier.addProduct(new Product("P1"), 1L);
+			metier.addProduct(new Product("P2"), 1L);
+			List<Product> prod2 = metier.listproduct();
+			assertTrue(prod1.size() == prod2.size() + 2);
 		} catch (Exception e) {
 			assertTrue(e.getMessage(), false);
 		}
