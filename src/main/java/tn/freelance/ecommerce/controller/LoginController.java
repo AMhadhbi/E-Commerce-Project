7bb86126f66c1b901ab89cf6dc2a0472ad4868c0
AMhadhbi <mhadhbi.achref@gmail.com>
2015-03-20 23:34:41 +0100

    Create class login Controller

---
+++ src/main/java/tn/freelance/ecommerce/controller/LoginController.java
@@ -0,0 +1,19 @@
+package tn.freelance.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping("/logout")
+	public String lgout() {
+		return "login";
+	}
+}
