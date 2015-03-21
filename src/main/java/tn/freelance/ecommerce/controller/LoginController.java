b977d9ccf506afd25f0a74fe8bbdd6955e859f52
AMhadhbi <mhadhbi.achref@gmail.com>
2015-03-21 16:44:27 +0100

    commit the application context

--- src/main/java/tn/freelance/ecommerce/controller/LoginController.java
+++ src/main/java/tn/freelance/ecommerce/controller/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	
-	
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
